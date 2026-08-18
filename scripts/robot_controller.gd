bcea3b764b006a36c971ec0bf5e3d2a073c2cff1
Kyle Woodhouse <woodkyle099@gmail.com>
2026-08-18 15:55:36 +0100

    Align robot navigation path with warehouse floor

--- scripts/robot_controller.gd
+++ scripts/robot_controller.gd
@@ -22,6 +22,15 @@ func _ready() -> void:
 
 func set_navigation_target(target: Vector3) -> void:
 	print("Robot set_navigation_target called: %s" % _format_vector3(target))
+	var navigation_map := navigation_agent.get_navigation_map()
+	var navigation_origin := NavigationServer3D.map_get_closest_point(
+		navigation_map,
+		global_position
+	)
+	var height_offset := navigation_origin.y - global_position.y
+	navigation_agent.path_height_offset = height_offset
+	print("Navigation origin: %s" % _format_vector3(navigation_origin))
+	print("Navigation path height offset: %.3f" % height_offset)
 	_destination = Vector3(target.x, global_position.y, target.z)
 	_has_target = true
 	_path_diagnostics_pending = true
@@ -39,8 +48,17 @@ func _physics_process(delta: float) -> void:
 	var next_position := navigation_agent.get_next_path_position()
 	if _path_diagnostics_pending:
 		_path_diagnostics_pending = false
+		var initial_horizontal_waypoint_distance := Vector2(
+			next_position.x - global_position.x,
+			next_position.z - global_position.z
+		).length()
+		print("Next path position: %s" % _format_vector3(next_position))
+		print("Robot position at path start: %s" % _format_vector3(global_position))
+		print("Initial horizontal waypoint distance: %.3f" % initial_horizontal_waypoint_distance)
 		print("Navigation final position: %s" % _format_vector3(navigation_agent.get_final_position()))
 		print("Target reachable: %s" % _format_bool(navigation_agent.is_target_reachable()))
+		if initial_horizontal_waypoint_distance <= 0.0001:
+			_print_zero_horizontal_waypoint_diagnostics()
 
 	var horizontal_remaining := _get_horizontal_remaining()
 	if horizontal_remaining <= navigation_agent.target_desired_distance:
@@ -100,6 +118,15 @@ func _print_finished_early_diagnostics(horizontal_remaining: float) -> void:
 	print("Navigation final position: %s" % _format_vector3(navigation_agent.get_final_position()))
 	print("Horizontal distance remaining: %.3f" % horizontal_remaining)
 	print("Target reachable: %s" % _format_bool(navigation_agent.is_target_reachable()))
+
+
+func _print_zero_horizontal_waypoint_diagnostics() -> void:
+	var current_path := navigation_agent.get_current_navigation_path()
+	print("Initial waypoint has zero horizontal distance")
+	print("Navigation path index: %d" % navigation_agent.get_current_navigation_path_index())
+	print("Navigation path point count: %d" % current_path.size())
+	for point_index in current_path.size():
+		print("Navigation path point %d: %s" % [point_index, _format_vector3(current_path[point_index])])
 
 
 func _format_vector3(value: Vector3) -> String:
